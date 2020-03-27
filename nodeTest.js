3888f338a6f5337d9d72b59b136988590f5f6020
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-03-27 16:09:17 -0600

    Lecture setup

---
+++ nodeTest.js
@@ -0,0 +1,16 @@
+const andrew = {
+  name: 'andrew',
+  age: 27,
+  favorites: {
+    color: 'red',
+    number: 38,
+    person: 'Becca',
+  },
+}
+
+function sayMyName(name) {
+  console.log(`My name is ${name}`)
+}
+
+// sayMyName(andrew.name)
+// console.log('THIS TOO')
